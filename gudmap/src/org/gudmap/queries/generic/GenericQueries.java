24628a557b8de1327b90790d8a20215bdd983281
Derek Houghton <dhoughto@inchmurrin.hgu.mrc.ac.uk>
2015-08-07 12:54:27 +0100

    homepage dialogs

--- gudmap/src/org/gudmap/queries/generic/GenericQueries.java
+++ gudmap/src/org/gudmap/queries/generic/GenericQueries.java
@@ -273,6 +273,6 @@ public class GenericQueries {
    				"WHERE STG_OID = SUB_STAGE_FK AND STG_SPECIES = ? ORDER BY STG_ORDER";
    		
    		//update statistics
-   		public final static String UPDATE_INFO = "SELECT MIS_SOFT_UPDATE software_update, MIS_EDIT_UPDATE editorial_update, MIS_SOFT_VERSION software_version FROM REF_MISC";
+   		public final static String UPDATE_INFO = "SELECT DATE_FORMAT(MIS_SOFT_UPDATE,'%e %M %Y') software_update, DATE_FORMAT(MIS_EDIT_UPDATE,'%e %M %Y') editorial_update, MIS_SOFT_VERSION software_version FROM REF_MISC";
 	   
 }
