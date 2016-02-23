95060f09797cb6f4ef33e4a984668b64d45b2d9b
Derek Houghton <dhoughto@inchmurrin.hgu.mrc.ac.uk>
2016-02-23 10:25:39 +0000

    chart stuff

--- gudmap/src/org/gudmap/models/ChartModel.java
+++ gudmap/src/org/gudmap/models/ChartModel.java
@@ -25,6 +25,18 @@ public class ChartModel {
 	private double microarray_lab = 0.0;
 	private double sequence_lab = 0.0;
 	
+	private int tot_met_entries = 0;
+	private int tot_met_genes = 0;
+	private int tot_lut_entries = 0;
+	private int tot_lut_genes = 0;
+	private int tot_ers_entries = 0;
+	private int tot_ers_genes = 0;
+	private int tot_frs_entries = 0;
+	private int tot_frs_genes = 0;
+	private int tot_mrs_entries = 0;
+	private int tot_mrs_genes = 0;
+	
+	
 	public ChartModel(){
 		
 	}
@@ -171,7 +183,75 @@ public class ChartModel {
 		return sequence_lab;
 	}
 	
+	public void setTot_met_entries(int tot_met_entries) {
+		this.tot_met_entries = tot_met_entries;
+	}
+	public int getTot_met_entries() {
+		return tot_met_entries;
+	}
 	
+	public void setTot_lut_entries(int tot_lut_entries) {
+		this.tot_lut_entries = tot_lut_entries;
+	}
+	public int getTot_lut_entries() {
+		return tot_lut_entries;
+	}
+	
+	public void setTot_ers_entries(int tot_ers_entries) {
+		this.tot_ers_entries = tot_ers_entries;
+	}
+	public int getTot_ers_entries() {
+		return tot_ers_entries;
+	}
+	
+	public void setTot_frs_entries(int tot_frs_entries) {
+		this.tot_frs_entries = tot_frs_entries;
+	}
+	public int getTot_frs_entries() {
+		return tot_frs_entries;
+	}
+	
+	public void setTot_mrs_entries(int tot_mrs_entries) {
+		this.tot_mrs_entries = tot_mrs_entries;
+	}
+	public int getTot_mrs_entries() {
+		return tot_mrs_entries;
+	}
+	
+	public void setTot_met_genes(int tot_met_genes) {
+		this.tot_met_genes = tot_met_genes;
+	}
+	public int getTot_met_genes() {
+		return tot_met_genes;
+	}
+	
+	public void setTot_ers_genes(int tot_ers_genes) {
+		this.tot_ers_genes = tot_ers_genes;
+	}
+	public int getTot_ers_genes() {
+		return tot_ers_genes;
+	}
+	
+	public void setTot_lut_genes(int tot_lut_genes) {
+		this.tot_lut_genes = tot_lut_genes;
+	}
+	public int getTot_lut_genes() {
+		return tot_lut_genes;
+	}
+	
+	public void setTot_frs_genes(int tot_frs_genes) {
+		this.tot_frs_genes = tot_frs_genes;
+	}
+	public int getTot_frs_genes() {
+		return tot_frs_genes;
+	}
+	
+	public void setTot_mrs_genes(int tot_mrs_genes) {
+		this.tot_mrs_genes = tot_mrs_genes;
+	}
+	public int getTot_mrs_genes() {
+		return tot_mrs_genes;
+	}
 	
 }
 	
